3b99599fb98d2e66a9539c99dfd39206946bb445
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-04-18 14:53:59 +0530

    task-management

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required:true}) name?:string ; 
+  @Input({required:true}) userId!:string;
+
+    isAddingTask =false;
+    
+ constructor(private tasksService:TasksService){
+ 
+ }
+
+  get selectedUserTask(){
+   return this.tasksService.getUserTasks(this.userId);
+  }
+
+  // onCompleteTask(id:string){
+  //   this.tasksService.removeTask(id);
+  // }
+  
+  onStartaddTask(){
+    this.isAddingTask=true;
+  }
+
+  onCloseAddTask(){
+    this.isAddingTask=false;
+  }
+
+
+ 
+ 
+}
